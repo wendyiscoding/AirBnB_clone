c5c16153218c3a3715f06fb9973c4ccc09892d75
BennettDixon <bennettdixon16@gmail.com>
2019-02-13 15:58:01 +0000

    added set_from_dict method and kwarg use to setattr for basemodels

--- models/base_model.py
+++ models/base_model.py
@@ -8,12 +8,15 @@ class BaseModel():
        used in project
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """init method for base class used in instantiation
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if len(kwargs) > 1:
+            self.set_from_dict(**kwargs)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """custom str method for str and print
@@ -37,3 +40,15 @@ class BaseModel():
         temp_d['created_at'] = self.created_at.isoformat()
         temp_d['updated_at'] = self.updated_at.isoformat()
         return temp_d
+
+    def set_from_dict(self, **kwargs):
+        """sets attributes from dictionary
+        """
+        for (k, v) in kwargs.items():
+            if "__" not in k:
+                if k in ('created_at', 'updated_at'):
+                    self.__dict__[k] = datetime.datetime\
+                                       .strptime(v,
+                                                 "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[k] = v
