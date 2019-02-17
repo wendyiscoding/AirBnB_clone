93b65cb3065c454d3bd271a0e20c9bdc0758db63
Wendy Leung <wendy.leung@holbertonschool.com>
2019-02-17 02:00:15 +0000

    add class Review

--- console.py
+++ console.py
@@ -8,6 +8,7 @@ from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
 from models.place import Place
+from models.review import Review
 from models.state import State
 from models.user import User
 
@@ -16,7 +17,7 @@ class HBNBCommand(cmd.Cmd):
     """class HBNBCommand
     """
     prompt = '(hbnb) '
-    class_list = ['BaseModel', 'User', 'State', 'City', 'Amenity', 'Place']
+    class_list = ['BaseModel', 'User', 'State', 'City', 'Amenity', 'Place', 'Review']
 
     def do_EOF(self, args):
         """EOF command to exit the program
