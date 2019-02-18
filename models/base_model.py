e2608d8800fa7cd77bbe44563f95654638601433
BennettDixon <bennettdixon16@gmail.com>
2019-02-18 19:46:45 +0000

    fixed basemodel init to only add to storage when not from kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -22,7 +22,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
-        models.storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """custom str method for str and print
